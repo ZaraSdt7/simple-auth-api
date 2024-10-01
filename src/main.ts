0c911b90defc88287f0254b2970bb5c17e5c4400
zahra <zahra.st7373@gmail.com>
2024-10-01 10:26:18 +0330

    test corse

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: false,
+    cors: { origin: '*', allowedHeaders: '*', methods: '*' },
   });
+  // app.enableCors({
+  //   origin: 'http://127.0.0.1:5173',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
 
   await app.listen(3000);
 }
